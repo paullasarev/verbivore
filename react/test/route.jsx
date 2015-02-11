ac8652798ba8515e94d683ba06d42ca0b42e2347
Pavel Lasarev <paullasarev@gmail.com>
2015-02-11 09:41:53 +0400

    fix login route test

--- react/test/route.jsx
+++ react/test/route.jsx
@@ -2,25 +2,19 @@ describe('router', function() {
 
   var assert = require('assert');
   var React = require('react/addons');
-  // var Router = require('react-router');
-  // var Route = Router.Route;
+  var TestUtils = React.addons.TestUtils;
+
   var router = require('../app/scripts/router.jsx');
-  var Login = require('../app/scripts/components/login.jsx');
+  // var Login = require('../app/scripts/components/login.jsx');
   process.env.NODE_ENV = 'test';
 
-  // var TestUtils = React.addons.TestUtils;
-  // var $ = require('jquery');
-
-  it('should handle login when /login path', function(done) {
+  it('should render login when /login path', function(done) {
 
     router.run('/login', function (Handler) {
-      // console.log('Handler:' + Handler.toString())
-      // console.log('Login:' + Login.toString())
-      var str1 = React.renderToString(<Handler/>);
-      var str2 = React.renderToString(<Login/>);
-      console.log(str1)
-      console.log(str2)
-      assert.ok(str1.indexOf(str2) >= 0);
+      var doc = TestUtils.renderIntoDocument(<Handler />);
+      var el = TestUtils.findRenderedDOMComponentWithClass(doc, 'login');
+      assert.ok(TestUtils.isDOMComponent(el));
+      // var str1 = React.renderToString(<Handler/>);
       done();
     });
 

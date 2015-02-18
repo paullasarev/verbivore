241c6709661bd9b5a8dc51f7d55a4e7bae3aefdc
RUSHAD_NABIULLIN <Rushad_Nabiullin@epam.com>
2015-02-18 18:41:55 +0400

    accordion table started

--- webapp/app/scripts/components/home.jsx
+++ webapp/app/scripts/components/home.jsx
@@ -7,16 +7,50 @@ var HomeFactory = function(mixins) {
     render: function() {
 
       return (
-        <div className="hero-unit">
-          <h1>'Allo, 'Allo!</h1>
-          <p>You now have</p>
-          <ul>
-              <li>ReactJS Reflux Boilerplate</li>
-              <li>Modernizr</li>
-              <li>Sass with Compass</li>
-              <li>Jest</li>
-          </ul>
-        </div>
+        <table className="accordion-table">
+          <tr className="accordion-row">
+            <td className="accordion-cell" rowSpan="10">test 1</td>
+            <td className="accordion-cell" >Row 1 Col 1</td>
+            <td className="accordion-cell" rowSpan="10">test 2</td>
+            <td className="accordion-cell" >Row 1 Col 2</td>
+          </tr>
+          <tr>
+            <td className="accordion-cell" >Row 2 Col 1</td>
+            <td className="accordion-cell" >Row 2 Col 2</td>
+          </tr>
+          <tr>
+            <td className="accordion-cell" >Row 3 Col 1</td>
+            <td className="accordion-cell" >Row 3 Col 2</td>
+          </tr>
+          <tr>
+            <td className="accordion-cell" >Row 4 Col 1</td>
+            <td className="accordion-cell" >Row 4 Col 2</td>
+          </tr>
+          <tr>
+            <td className="accordion-cell" >Row 5 Col 1</td>
+            <td className="accordion-cell" >Row 5 Col 2</td>
+          </tr>
+          <tr>
+            <td className="accordion-cell" >Row 6 Col 1</td>
+            <td className="accordion-cell" >Row 6 Col 2</td>
+          </tr>
+          <tr>
+            <td className="accordion-cell" >Row 7 Col 1</td>
+            <td className="accordion-cell" >Row 7 Col 2</td>
+          </tr>
+          <tr>
+            <td className="accordion-cell" >Row 8 Col 1</td>
+            <td className="accordion-cell" >Row 8 Col 2</td>
+          </tr>
+          <tr>
+            <td className="accordion-cell" >Row 9 Col 1</td>
+            <td className="accordion-cell" >Row 9 Col 2</td>
+          </tr>
+          <tr>
+            <td className="accordion-cell" >Row 10 Col 1</td>
+            <td className="accordion-cell" >Row 10 Col 2</td>
+          </tr>
+        </table>
       );
     }
   });
